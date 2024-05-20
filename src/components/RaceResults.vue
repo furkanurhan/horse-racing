fd8e22293d5e4f904e46bb5cbe3191996be3ade0
furkanurhan <urhanfurkan35@gmail.com>
2024-05-20 04:23:12 +0300

    Development finished

---
+++ src/components/RaceResults.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="race-results">
+    <h2 style="background-color: #1ba71b; padding: 8px;">Results</h2>
+    <div class="table-responsive">
+      <table>
+        <tbody>
+          <template v-for="(race, rI) in results">
+            <tr>
+              <td colspan="2" style="background-color: red; text-align: center;">{{ `${rI + 1}. Lap - ${race.distance}m` }}</td>
+            </tr>
+            <tr>
+              <td class="table--th">Position</td>
+              <td class="table--th">Name</td>
+            </tr>
+            <template v-for="(horse, hI) in race.result">
+              <tr>
+                <td :style="{ 'background-color': hI === 0 ? '#2eba2e' : 'unset'}" >{{ hI + 1 }}</td>
+                <td :style="{ 'background-color': hI === 0 ? '#2eba2e' : 'unset'}" style="text-align: center;">{{ horse.name }}</td>
+              </tr>
+            </template>
+          </template>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
+const store = useStore()
+
+const horses = computed(() => store.state.racing.horses)
+const races = computed(() => store.state.racing.races)
+const results = computed(() => store.state.racing.results)
+const currentRaceIndex = computed(() => store.state.racing.currentRaceIndex)
+
+const startRaces = () => {
+  runRace(currentRaceIndex.value)
+}
+
+const runRace = (index) => {
+  const width = document.getElementsByClassName("runway")[0].offsetWidth
+  
+  let intervalID = setInterval(() => {
+    races.value[index].horses.forEach((horse, index) => {
+      horse.location = (horse.location + (horse.condition / (10 / (width / 100)))) > width ? width : (horse.location + (horse.condition / (10 / (width / 100))))
+    })
+
+    if (races.value[index].horses.some(horse => horse.location >= (width - 100))) {
+      clearInterval(intervalID)
+
+      const horsesTemp = [...races.value[index].horses]
+      const sortedHorses = horsesTemp.sort((a, b) => b.condition - a.condition)
+      races.value[index].result = sortedHorses
+      store.dispatch('racing/fetchResults', races.value[index])
+
+      store.dispatch('racing/incrementCurrentRaceIndex')
+
+      if(currentRaceIndex.value < 6) {
+        runRace(currentRaceIndex.value)
+      }
+    }
+  }, 500)
+}
+
+defineExpose({
+  startRaces
+})
+</script>
+
+<style scoped>
+.table-responsive {
+  overflow-y: auto;
+  max-height: 600px;
+}
+
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+.table--th {
+  background-color: #dddddd;
+}
+
+td, th {
+  border: 1px solid #bababa;
+  text-align: left;
+  padding: 8px;
+}
+</style>
---
+++ src/components/RaceSchedule.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="race-schedule">
+    <h2 style="background-color: #4a4aff; padding: 8px;">Program</h2>
+    <div class="table-responsive">
+      <table>
+        <tbody>
+          <template v-for="(race, rI) in races">
+            <tr>
+              <td colspan="2" style="background-color: red; text-align: center;">{{ `${rI + 1}. Lap - ${race.distance}m` }}</td>
+            </tr>
+            <tr>
+              <td class="table--th">Position</td>
+              <td class="table--th">Name</td>
+            </tr>
+            <template v-for="(horse, hI) in race.horses">
+              <tr>
+                <td>{{ hI + 1 }}</td>
+                <td style="text-align: center;">{{ horse.name }}</td>
+              </tr>
+            </template>
+          </template>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+
+<script setup>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
+const store = useStore()
+
+const horses = computed(() => store.state.racing.horses)
+const races = computed(() => store.state.racing.races)
+
+const getRandomHorses = (n) => {
+  const horsesTemp = []
+  const availableHorses = [...horses.value]
+  for (let i = 0; i < n; i++) {
+    const randomIndex = Math.floor(Math.random() * availableHorses.length)
+    const selectedHorse = availableHorses.splice(randomIndex, 1)[0]
+    horsesTemp.push({...selectedHorse, location: 0})
+  }
+  return horsesTemp
+}
+
+const generateRaces = () => {
+  const racesTemp = []
+  for (let i = 0; i < 6; i++) {
+    const distance = [1200, 1400, 1600, 1800, 2000, 2200][i]
+    const horsesRandom = getRandomHorses(10)
+    racesTemp.push({
+      id: i + 1,
+      distance: distance,
+      horses: horsesRandom,
+      result: null,
+    })
+  }
+  store.dispatch('racing/fetchRaces', racesTemp)
+}
+
+defineExpose({
+  generateRaces
+})
+
+</script>
+
+<style scoped>
+.table-responsive {
+  overflow-y: auto;
+  max-height: 600px;
+}
+
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+.table--th {
+  background-color: #dddddd;
+}
+
+td, th {
+  border: 1px solid #bababa;
+  text-align: left;
+  padding: 8px;
+}
+</style>
